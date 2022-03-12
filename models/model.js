9daec8f9507295d48a85cc44a8b6b6ab5be38d36
Madrog <rogers.madda@gmail.com>
2022-03-12 18:58:36 +0300

    Implemented full CRUD functionality

--- models/model.js
+++ models/model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const dataSchema = new mogoose.Schema({
+const dataSchema = new mongoose.Schema({
     name : {
         required: true,
         type: String
